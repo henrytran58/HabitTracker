012bce8fbcdf3ff9c87c2813f0d6062283460cec
henrytran58 <henry.ndk.tran@gmail.com>
2025-06-21 03:57:25 -0400

    used railway database

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:henrytran@localhost/habittracker'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:henrytran@localhost/habittracker'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 

2619897ccdf2f648854f89f17c5bc58fcc4600b1
henrytran58 <henry.ndk.tran@gmail.com>
2025-06-21 21:04:04 -0400

    updated python version

--- backend/build.sh
+++ backend/build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
+# set -e
 
-echo "--- Checking Python version ---"
-python --version
-python3 --version
+# echo "--- Checking Python version ---"
+# python --version
+# python3 --version
 
-echo "--- Updating pip ---"
-pip install --upgrade pip
+# echo "--- Updating pip ---"
+# pip install --upgrade pip
 
-echo "--- Installing/Upgrading setuptools and wheel ---"
-# Reinstalling setuptools can sometimes clear out lingering older files
-pip install --upgrade --force-reinstall setuptools wheel
+# echo "--- Installing/Upgrading setuptools and wheel ---"
+# # Reinstalling setuptools can sometimes clear out lingering older files
+# pip install --upgrade --force-reinstall setuptools wheel
 
 echo "--- Installing project dependencies ---"
-pip install --upgrade --upgrade-strategy eager -r requirements.txt
+pip install -r requirements.txt
 
 # If you have other build steps, add them here
 # For example:

4071c22eaa7c6dc4747fceacf3b4bed89814aa32
henrytran58 <henry.ndk.tran@gmail.com>
2025-06-21 20:13:43 -0400

    added build.sh

---
+++ backend/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+echo "--- Updating pip, setuptools, and wheel ---"
+pip install --upgrade pip setuptools wheel
+
+echo "--- Installing project dependencies ---"
+pip install -r requirements.txt
+
+# If you have other build steps, add them here
+# For example:
+# python manage.py collectstatic --noinput
+# python manage.py migrate
